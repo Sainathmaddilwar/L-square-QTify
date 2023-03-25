126ac53df2f4d260a72ca74400f815b4ec9d6e18
Sainath Shankarrao Maddilwar (APMEA - iCORE-CRS) <SA20294714@wipro.com>
2023-03-24 23:06:39 -0700

    router

--- qtify/src/components/Carousel/CarouselLeftArrow/CarouselLeftArrow.jsx
+++ qtify/src/components/Carousel/CarouselLeftArrow/CarouselLeftArrow.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useSwiper, useSwiperSlide } from "swiper/react";
+import { useSwiper } from "swiper/react";
 import styles from "../CarouselLeftArrow/CarouselLeftArrow.module.css";
 import { ReactComponent as LeftArrow } from "../../../assets/LeftArrow.svg";
 
